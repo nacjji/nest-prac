f8204d44145390ee7a669286372543a450b08c92
jiwon na <jiwon.na@fig.xyz>
2023-08-01 17:31:46 +0900

    ADD : user entity 추가

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,10 @@
-export class User {}
+import { Column, Entity } from 'typeorm';
+import { CommonPkEntity } from './common/common.entity';
+
+@Entity('User')
+export class UserEntity extends CommonPkEntity {
+  @Column('varchar', { unique: false, nullable: false })
+  email: string;
+  @Column('varchar', { unique: false, nullable: false })
+  password: string;
+}
